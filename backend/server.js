32b878b06ade9943e1dff7f65fcf6cc056efe8bb
Joey Huang <joeyh6547@gmail.com>
2024-01-09 22:56:25 -0500

    server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const workoutRoutes = require('./routes/workouts');
-const calorieRoutes = require('./routes/calories');
-const musicRoutes = require('./routes/music');
+//const calorieRoutes = require('./routes/calories');
 
 
 // express app
@@ -17,10 +18,13 @@ app.use((req, res, next) => {
 })
 
 // workout routes
-app.use('api/GymPal/workouts/', workoutRoutes);
+app.use('/api/GymPal/workouts/', workoutRoutes);
 // calorie routes
-app.use('api/GymPal/calories/', calorieRoutes);
-// music routes
-app.use('api/GymPal/music/', musicRoutes);
+//app.use('api/GymPal/calories/', calorieRoutes);
 
-app.listen(process.env.PORT, () => {console.log(`Listening on port ${process.env.PORT}`)});
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {console.log(`Connected to database. Listening on port ${process.env.PORT}`)});
+    })
+    .catch((error) => {console.log(error)});

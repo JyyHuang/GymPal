cf1e26c88c78aef53c6ec8e44f49c3aba672edd4
Joey Huang <joeyh6547@gmail.com>
2024-01-09 23:58:30 -0500

    user form

---
+++ frontend/app/components/WorkoutForm.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [workoutName, setWorkoutName] = useState('');
+    const [sets, setSets] = useState('');
+    const [reps, setReps] = useState('');
+    const [weight, setWeight] = useState('');
+}

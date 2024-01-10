2d1ce6d33b675658f3392cede45801c602e21a38
Joey Huang <joeyh6547@gmail.com>
2024-01-09 23:32:41 -0500

    nutrition tab

---
+++ frontend/app/(tabs)/nutrition.js
@@ -0,0 +1,11 @@
+const { View, Text } = require("react-native")
+
+const nutritionPage = () => {
+    return (
+    <View>
+        <Text>Hello</Text>
+    </View>
+    );
+};
+
+export default nutritionPage;

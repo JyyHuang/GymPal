88e1586d6fbf942a9e3582d6d11870018a6c8db1
Joey Huang <joeyh6547@gmail.com>
2024-01-13 14:59:07 -0500

    Tech Stack

--- frontend/app/components/WorkoutForm.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const WorkoutForm = () => {
-    const [workoutName, setWorkoutName] = useState('');
-    const [sets, setSets] = useState('');
-    const [reps, setReps] = useState('');
-    const [weight, setWeight] = useState('');
-}

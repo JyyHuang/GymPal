36a56639f5f6675747c3b6c4ba22b002aa4db38e
Joey Huang <joeyh6547@gmail.com>
2024-01-13 14:25:04 -0500

    Tabs

--- frontend/app/(tabs)/_layout.js
+++ frontend/app/(tabs)/_layout.js
@@ -1,29 +1,57 @@
 const { Tabs } = require("expo-router")
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { WorkoutContextProvider } from '../../context/WorkoutContext';
+import { StatusBar } from 'react-native';
+import {SafeAreaView } from 'react-native-safe-area-context';
 
 
 const TabsLayout = () => {
 
     return (
-        <Tabs>
-            <Tabs.Screen name="index" 
-                options={{
-                    headerTitle: "Workouts",
-                    title: "Workouts",
-                    tabBarIcon: ({focused}) => {
-                        const iconName = focused ? 'barbell' : 'barbell-outline';
-                        return <Ionicons name={iconName} size={24} color='black'/>}
-                    }}/>
-            <Tabs.Screen name="nutrition" 
-                options={{
-                    headerTitle: "Nutrition",
-                    title: "Nutrition",
-                    tabBarIcon: ({focused}) => {
-                        const iconName = focused ? 'nutrition' : 'nutrition-outline';
-                        return <Ionicons name={iconName} size={24} color='black'/>}
-                    }}>
-            </Tabs.Screen>
-        </Tabs>
+        
+        <WorkoutContextProvider>
+            <SafeAreaView><StatusBar></StatusBar></SafeAreaView>
+            <Tabs >
+                <Tabs.Screen name="index" 
+                    options={{
+                        headerTitle: "Workouts",
+                        headerStyle: {
+                            backgroundColor:'black'
+                        },
+                        headerTitleStyle: {
+                            color:'white'
+                        },
+                        title: "Workouts",
+                        tabBarActiveBackgroundColor:'black',
+                        tabBarInactiveBackgroundColor: 'black',
+                        tabBarActiveTintColor:'white',
+                        tabBarInactiveTintColor:'gray',
+                        tabBarIcon: ({focused}) => {
+                            const iconName = focused ? 'barbell' : 'barbell-outline';
+                            return <Ionicons name={iconName} size={24} color='white'/>}
+                        }}
+                        />
+                <Tabs.Screen name="nutrition" 
+                    options={{
+                        headerTitle: "Nutrition",
+                        headerStyle: {
+                            backgroundColor:'black'
+                        },
+                        headerTitleStyle: {
+                            color:'white'
+                        },
+                        title: "Nutrition",
+                        tabBarActiveBackgroundColor:'black',
+                        tabBarInactiveBackgroundColor: 'black',
+                        tabBarActiveTintColor:'white',
+                        tabBarInactiveTintColor:'gray',
+                        tabBarIcon: ({focused}) => {
+                            const iconName = focused ? 'nutrition' : 'nutrition-outline';
+                            return <Ionicons name={iconName} size={24} color='white'/>}
+                        }}>
+                </Tabs.Screen>
+            </Tabs>
+        </WorkoutContextProvider>
     );
 };
 

7bed4d77a0d06a534a0b939ee917dde5d4fb33c9
Joey Huang <joeyh6547@gmail.com>
2024-01-13 14:21:05 -0500

    homepage

--- frontend/app/(tabs)/index.js
+++ frontend/app/(tabs)/index.js
@@ -1,23 +1,29 @@
 import { FontAwesome5 } from '@expo/vector-icons';
-import { View, ScrollView, TouchableOpacity, Text } from "react-native";
+import { View, ScrollView, TouchableOpacity, Modal, Text } from "react-native";
 import { useEffect, useRef, useState } from "react";
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import CreateWorkoutForm from '../components/CreateWorkoutForm';
+import { useWorkoutContext } from '../../hooks/useWorkoutContext';
 
 // components
 import WorkoutInfo from '../components/WorkoutInfo';
 
 const WorkoutsPage = () => {
-    const [workouts, setWorkouts] = useState(null);
+    const {workouts, dispatch} = useWorkoutContext();
+    const [modal, setModal] = useState(false);
+
 
     // render workouts when first renders
     useEffect(() => {
         const fetchWorkouts = async () => {
             try{
                 const response = await fetch('http://10.0.2.2:3000/api/GymPal/workouts/');
-                const workoutJson = await response.json();
+                const workoutsJson = await response.json();
 
                 if (response.ok)
                 {
-                    setWorkouts(workoutJson);
+                    dispatch({type: 'SET_WORKOUTS', payload: workoutsJson})
                 }
             }
             catch(err) {
@@ -26,7 +32,7 @@ const WorkoutsPage = () => {
         }
 
         fetchWorkouts();
-    }, []);
+    }, [dispatch]);
 
     const scrollRef = useRef();
     const onPressTouch = () => {
@@ -37,20 +43,43 @@ const WorkoutsPage = () => {
     }
 
     return (
-        <View className='flex-1'>
-            <ScrollView ref={scrollRef}>
-                {workouts && workouts.map((workout) => (
-                    <WorkoutInfo key={workout._id} workout={workout} />
-                ))}
+            <View className='flex-1 bg-neutral-800'>
+                    
+                <Modal visible={modal} animationType='fade' transparent={true}>
+                    <View className='flex-1 bg-[#000000aa]'>
+                        <View className='m-5 bg-white rounded-2xl mt-40'>
+                            <TouchableOpacity onPress={() => setModal(false)}>
+                                <View className='items-center mt-2'>
+                                    <MaterialCommunityIcons name="close-circle-outline" size={24} color="black" />
+                                </View>
+                            </TouchableOpacity>
+                            <CreateWorkoutForm setModal={setModal}/>
+                        </View>
+                    </View>
+                </Modal>
                 
-            </ScrollView>
+                <ScrollView ref={scrollRef}>
+
+                    <TouchableOpacity onPress={() => setModal(true)}>
+                        <View className='absolute right-2 top-2'>
+                            <MaterialIcons name="add-circle-outline" size={24} color="white" />
+                        </View>
+                    </TouchableOpacity>
+
+                    <View className="mt-8"></View>
+                    {workouts && workouts.map((workout) => (
+                        <WorkoutInfo key={workout._id} workout={workout} />
+                    ))}
+
+                </ScrollView>
 
                 <TouchableOpacity onPress={onPressTouch}>
                     <View className='absolute right-10 bottom-10'>
-                        <FontAwesome5 name="arrow-circle-up" size={26} color="black" />
+                        <FontAwesome5 name="arrow-circle-up" size={26} color='white'/>
                     </View>
                 </TouchableOpacity>
-        </View>
+
+            </View>
     );
 };
 

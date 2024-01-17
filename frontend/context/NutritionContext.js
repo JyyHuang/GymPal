f34b0a159e52f764dd6ad8bb87fca3cc3823883e
Joey Huang <joeyh6547@gmail.com>
2024-01-16 22:23:04 -0500

    Added Redux style nutrition context

---
+++ frontend/context/NutritionContext.js
@@ -0,0 +1,47 @@
+import { createContext, useReducer } from "react";
+
+// State for foods page
+export const NutritionContext = createContext();
+
+export const nutritionReducer = (state, action) => {
+    switch (action.type) {
+        case "SET_NUTRITION":
+            return {
+                nutrition: action.payload
+            }
+        case "SEARCH_NUTRITION":
+            return{
+                nutrition: action.payload
+            }
+        case "OPEN_SEARCH_MODAL":
+            return {
+                nutrition: null
+            }
+        case "CREATE_NUTRITION":
+            return {
+                nutrition: [action.payload, ...state.nutrition]
+            }
+        case "DELETE_NUTRITION":
+            return {
+                nutrition: state.nutrition.filter((n) => n._id !== action.payload._id)
+            }
+        case "DELETE_ALL_NUTRITION":
+            return {
+                nutrition: null
+            }
+        default:
+            return state;
+    }
+}
+
+export const NutritionContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(nutritionReducer, {
+        nutrition: null
+    });
+
+    return (
+        <NutritionContext.Provider value={{...state, dispatch}}>
+            {children}
+        </NutritionContext.Provider>
+    )
+} 

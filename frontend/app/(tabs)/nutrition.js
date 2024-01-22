7ac8810ffec7e1d5c6cadfb6f7b1f603ceeead0e
Joey Huang <joeyh6547@gmail.com>
2024-01-21 23:30:26 -0500

    Nutrition tab

--- frontend/app/(tabs)/nutrition.js
+++ frontend/app/(tabs)/nutrition.js
@@ -1,37 +1,93 @@
-import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { View, Text, TouchableOpacity, Modal, ScrollView, TouchableWithoutFeedback, KeyboardAvoidingView } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 import {useNutritionContext} from '../../hooks/useNutritionContext';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateFoodItemForm from "../components/CreateFoodItemForm";
 
 const nutritionPage = () => {
     const {nutrition, dispatch} = useNutritionContext();
     const [addModal, setAddModal] = useState(false);
+    let totalCalories = 0;
+    let totalProtein = 0;
+    let totalFat = 0;
+    
+    // render nutrition when first renders
+    useEffect(() => {
+        const fetchNutrition = async () => {
+            try{
+                const response = await fetch('http://10.0.2.2:3000/api/GymPal/nutrition/',{
+                    method:"GET"
+                });
+
+                const nutritionJson = await response.json();
+
+                if (response.ok)
+                {
+                    dispatch({type: "SET_NUTRITION", payload: nutritionJson})
+                }
+            }
+            catch(err) {
+                console.log(err)
+            }
+        }
+        fetchNutrition();
+    }, [dispatch]);
+
+    {nutrition && nutrition.forEach((n) => {
+        totalCalories += n.calories;
+        totalFat += n.fat;
+        totalProtein += n.protein
+        }
+    )
+    } 
 
     return (
     <View className='flex-1 bg-neutral-800'>
         
+        
         <Modal visible={addModal} animationType='fade' transparent={true}>
-            <View className='flex-1 bg-[#000000aa]'>
-                <CreateFoodItemForm setAddModal={setAddModal}/>
-                <View onTouchEnd={() => {setAddModal(false)}} className='flex-1'></View>
-            </View>
+            <TouchableWithoutFeedback onPressOut={() => {setAddModal(false)}}>
+                <View className='flex-1 bg-[#000000aa]'>
+                    <ScrollView>
+                        <CreateFoodItemForm setAddModal={setAddModal}/>
+                    </ScrollView>
+                </View>
+            </TouchableWithoutFeedback>
         </Modal>
+        
+        
 
         <View className='flex items-center'>
             <View className='absolute top-24'>
-                <TouchableOpacity onPress={() => {setAddModal(true); dispatch({type:"OPEN_SEARCH_MODAL"})}}>
+                <TouchableOpacity onPress={() => {setAddModal(true)}}>
                     <View>
                         <MaterialIcons name="add-circle-outline" size={30} color="white" />
                     </View>
                 </TouchableOpacity>
             </View>
         </View>
+        
         <View className='flex-1 justify-center items-center'>
-            <View className='border border-white w-full items-center justify-center h-1/2'>
-                <Text>s</Text>
-            </View>
+                <View className='items-center'>       
+                        <Text className='text-white text-6xl'>{Math.round((totalCalories + Number.EPSILON) * 100) / 100}</Text>
+                        <Text className='text-white'>Calories</Text>
+                     
+                </View>
+                
+                <View className='flex-row mt-5'>
+                    <View className='items-center m-10'>
+                        <Text className='text-white text-2xl'>{Math.round((totalProtein + Number.EPSILON) * 100) / 100}</Text>
+                        <Text className='text-white'>Protein (g)</Text>
+                    </View>
+
+
+                    <View className='items-center m-10'>
+                        <Text className='text-white text-2xl'>{Math.round((totalFat + Number.EPSILON) * 100) / 100}</Text>
+                        <Text className='text-white'>Fat (g)</Text>
+                    </View>
+                </View>
         </View>
+        
     </View>
     );
 };

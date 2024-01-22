6461304d4e19074eefaa603a6d1be64edc5b4e7c
Joey Huang <joeyh6547@gmail.com>
2024-01-21 23:31:08 -0500

    Add Food Form

--- frontend/app/components/CreateFoodItemForm.js
+++ frontend/app/components/CreateFoodItemForm.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import {Formik} from 'formik';
 import { TextInput, View, Text, TouchableOpacity } from "react-native";
-import { useNutritionContext } from '../../hooks/useNutritionContext';
 import { EvilIcons } from '@expo/vector-icons';
 import FoodSearchItem from './FoodSearchItem';
 
 const CreateFoodItemForm = ({setAddModal}) => {
-    const {nutrition, dispatch} = useNutritionContext();
+    const [searchItems, setSearchItems] = useState([]);
     const [error, setError] = useState(false);
     
+    let foodArray = [];
 
     return (
         <View>
@@ -17,26 +17,29 @@ const CreateFoodItemForm = ({setAddModal}) => {
                 onSubmit={ async (values) => {
                     
                     const response = await fetch(`http://10.0.2.2:3000/api/GymPal/nutrition/search?query=${values.searchItem}`,{
-                        method:'GET',
+                        method:'GET'
                         }
                     )
                     
                     const json = await response.json();
-
+                    
                     if (!response.ok){
                         setError(true)
                     }
 
                     if(response.ok){
-                        let foodArray = [];
 
                         setError(false)
                         for (let i = 0; i < json.length; i++){
                             let foodObject = {};
                             const fdcId = json[i].fdcId;
                             const description = json[i].description
+                            const servingSize = json[i].servingSize
+                            const servingSizeUnit = json[i].servingSizeUnit
                             let currObject = Object.assign(foodObject, {'fdcId': fdcId})
                             currObject = Object.assign(currObject, {'description': description})
+                            currObject = Object.assign(currObject, {'servingSize': servingSize})
+                            currObject = Object.assign(currObject, {'servingSizeUnit': servingSizeUnit})
                             
                             
                             for (let j = 0; j < json[i].foodNutrients.length; j++){
@@ -53,15 +56,14 @@ const CreateFoodItemForm = ({setAddModal}) => {
                             }
                             foodArray.push(currObject)
                         }
-                        
-                        dispatch({type: 'SEARCH_NUTRITION', payload: foodArray})
+                        setSearchItems(foodArray)
                     }
             }}>
 
             {(formikProps) => (
                 <View>
                     <TextInput 
-                        placeholder={error ? "Please Input A Search" : "Search..."}
+                        placeholder={error ? "Please Input A Valid Search" : "Search..."}
                         placeholderTextColor='white'
                         onChangeText={formikProps.handleChange('searchItem')}
                         value={formikProps.values.searchItem}
@@ -70,8 +72,8 @@ const CreateFoodItemForm = ({setAddModal}) => {
                     />
                     <TouchableOpacity className='absolute right-5 top-6' onPress={formikProps.handleSubmit}><EvilIcons name="search" size={24} color="white" /></TouchableOpacity>
                     <View>
-                        {nutrition && nutrition.map((food) => (
-                            <FoodSearchItem key={food.fdcId} food={food} />
+                        {searchItems && searchItems.map((food) => (
+                            <FoodSearchItem key={food.fdcId} food={food} setAddModal={setAddModal}/>
                             )
                         )}
                     </View>

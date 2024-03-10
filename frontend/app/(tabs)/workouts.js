f5e851e67e585c28d6b602f28a6567f85afd70b7
JyyHuang <jhuang6547@gmail.com>
2024-03-10 01:40:28 -0500

    Added conditional rendering to check user status

---
+++ frontend/app/(tabs)/workouts.js
@@ -0,0 +1,94 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import { View, ScrollView, TouchableOpacity, Modal } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import CreateWorkoutForm from '../components/CreateWorkoutForm';
+import { useWorkoutContext } from '../../hooks/useWorkoutContext';
+import { useAuthContext} from '../../hooks/useAuthContext'
+import CheckLogin from '../components/CheckLogin'
+
+// components
+import WorkoutInfo from '../components/WorkoutInfo';
+
+const WorkoutsPage = () => {
+    const {workouts, dispatch} = useWorkoutContext();
+    const [modal, setModal] = useState(false);
+    const {user} = useAuthContext();
+
+    // render workouts when first renders
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            try{
+                const response = await fetch('http://10.0.2.2:3000/api/GymPal/workouts/', {
+                    headers: {
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                });
+                const workoutsJson = await response.json();
+
+                if (response.ok)
+                {
+                    dispatch({type: 'SET_WORKOUTS', payload: workoutsJson})
+                }
+            }
+            catch(err) {
+                console.log(err)
+            }
+        }
+        if (user){
+            fetchWorkouts();
+        }
+    }, [dispatch, user]);
+
+    const scrollRef = useRef();
+    const onPressTouch = () => {
+        scrollRef.current?.scrollTo({
+          y: 0,
+          animated: true,
+        });
+    }
+
+    if (!user) return <CheckLogin></CheckLogin>
+    return (
+            <View className='flex-1 bg-neutral-800'>
+                    
+                <Modal visible={modal} animationType='fade' transparent={true}>
+                    <View className='flex-1 bg-[#000000aa]'>
+                        <View className='m-5 bg-white rounded-2xl mt-40'>
+                            <TouchableOpacity onPress={() => setModal(false)}>
+                                <View className='items-center mt-2'>
+                                    <MaterialCommunityIcons name="close-circle-outline" size={24} color="black" />
+                                </View>
+                            </TouchableOpacity>
+                            <CreateWorkoutForm setModal={setModal}/>
+                        </View>
+                    </View>
+                </Modal>
+                
+                <ScrollView ref={scrollRef}>
+
+                    <TouchableOpacity onPress={() => setModal(true)}>
+                        <View className='absolute right-2 top-2'>
+                            <MaterialIcons name="add-circle-outline" size={24} color="white" />
+                        </View>
+                    </TouchableOpacity>
+
+                    <View className="mt-8"></View>
+                    {workouts && workouts.map((workout) => (
+                        <WorkoutInfo key={workout._id} workout={workout} />
+                    ))}
+
+                </ScrollView>
+
+                <TouchableOpacity onPress={onPressTouch}>
+                    <View className='absolute right-10 bottom-10'>
+                        <FontAwesome5 name="arrow-circle-up" size={26} color='white'/>
+                    </View>
+                </TouchableOpacity>
+
+            </View>
+    );
+};
+
+export default WorkoutsPage;

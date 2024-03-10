ddcc24915e8f61bcfb67da404ab170cf1a8f499a
JyyHuang <jhuang6547@gmail.com>
2024-03-10 01:49:46 -0500

    Created Tab For Login And Signup

--- frontend/app/(tabs)/_layout.js
+++ frontend/app/(tabs)/_layout.js
@@ -1,62 +1,86 @@
 const { Tabs } = require("expo-router")
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { WorkoutContextProvider } from '../../context/WorkoutContext';
 import { NutritionContextProvider } from '../../context/NutritionContext';
+import { AuthContextProvider } from '../../context/AuthContext';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import {SafeAreaView } from 'react-native-safe-area-context';
 
 
 const TabsLayout = () => {
-
     return (
-        
-        <WorkoutContextProvider>
-        <NutritionContextProvider>
-            <SafeAreaView></SafeAreaView>
-            <Tabs >
-                <Tabs.Screen name="index" 
-                    options={{
-                        headerTitle: "Workouts",
-                        headerStyle: {
-                            backgroundColor:'black',
-                            
-                        },
-                        headerTitleStyle: {
-                            color:'white'
-                        },
-                        title: "Workouts",
-                        tabBarActiveBackgroundColor:'black',
-                        tabBarInactiveBackgroundColor: 'black',
-                        tabBarActiveTintColor:'white',
-                        tabBarInactiveTintColor:'gray',
-                        tabBarHideOnKeyboard: true,
-                        tabBarIcon: ({focused}) => {
-                            const iconName = focused ? 'barbell' : 'barbell-outline';
-                            return <Ionicons name={iconName} size={24} color='white'/>}
-                        }}
-                        />
-                <Tabs.Screen name="nutrition" 
-                    options={{
-                        headerTitle: "Nutrition",
-                        headerStyle: {
-                            backgroundColor:'black'
-                        },
-                        headerTitleStyle: {
-                            color:'white'
-                        },
-                        title: "Nutrition",
-                        tabBarActiveBackgroundColor:'black',
-                        tabBarInactiveBackgroundColor: 'black',
-                        tabBarActiveTintColor:'white',
-                        tabBarInactiveTintColor:'gray',
-                        tabBarHideOnKeyboard: true,
-                        tabBarIcon: ({focused}) => {
-                            const iconName = focused ? 'nutrition' : 'nutrition-outline';
-                            return <Ionicons name={iconName} size={24} color='white'/>}
-                        }}>
-                </Tabs.Screen>
-            </Tabs>
-        </NutritionContextProvider>
-        </WorkoutContextProvider>
+
+        <AuthContextProvider>
+            <WorkoutContextProvider>
+                <NutritionContextProvider>
+                    <SafeAreaView></SafeAreaView>
+                        <Tabs>
+                            <Tabs.Screen 
+                                name="index"
+                                options={{
+                                    headerTitle: "Account",
+                                    headerStyle: {
+                                        backgroundColor:'black',
+                                        
+                                    },
+                                    headerTitleStyle: {
+                                        color:'white'
+                                    },
+                                    title: "Account",
+                                    tabBarActiveBackgroundColor:'black',
+                                    tabBarInactiveBackgroundColor: 'black',
+                                    tabBarActiveTintColor:'white',
+                                    tabBarInactiveTintColor:'gray',
+                                    tabBarHideOnKeyboard: true,
+                                    tabBarIcon: ({focused}) => {
+                                        const iconName = focused ? 'person' : 'person-outline';
+                                        return <Ionicons name={iconName} size={24} color='white'/>}
+                                }}
+                                />
+                            <Tabs.Screen name="workouts"  
+                                options={{
+                                    headerTitle: "Workouts",
+                                    headerStyle: {
+                                        backgroundColor:'black',
+                                        
+                                    },
+                                    headerTitleStyle: {
+                                        color:'white'
+                                    },
+                                    title: "Workouts",
+                                    tabBarActiveBackgroundColor:'black',
+                                    tabBarInactiveBackgroundColor: 'black',
+                                    tabBarActiveTintColor:'white',
+                                    tabBarInactiveTintColor:'gray',
+                                    tabBarHideOnKeyboard: true,
+                                    tabBarIcon: ({focused}) => {
+                                        const iconName = focused ? 'barbell' : 'barbell-outline';
+                                        return <Ionicons name={iconName} size={24} color='white'/>}
+                                    }}
+                                    />
+                            <Tabs.Screen name="nutrition"
+                                options={{
+                                    headerTitle: "Nutrition",
+                                    headerStyle: {
+                                        backgroundColor:'black'
+                                    },
+                                    headerTitleStyle: {
+                                        color:'white'
+                                    },
+                                    title: "Nutrition",
+                                    tabBarActiveBackgroundColor:'black',
+                                    tabBarInactiveBackgroundColor: 'black',
+                                    tabBarActiveTintColor:'white',
+                                    tabBarInactiveTintColor:'gray',
+                                    tabBarHideOnKeyboard: true,
+                                    tabBarIcon: ({focused}) => {
+                                        const iconName = focused ? 'nutrition' : 'nutrition-outline';
+                                        return <Ionicons name={iconName} size={24} color='white'/>}
+                                    }}>
+                        </Tabs.Screen>
+                    </Tabs>
+                </NutritionContextProvider>
+            </WorkoutContextProvider>
+        </AuthContextProvider>
     );
 };
 
--- frontend/app/(tabs)/index.js
+++ frontend/app/(tabs)/index.js
@@ -1,86 +1,52 @@
-import { FontAwesome5 } from '@expo/vector-icons';
-import { View, ScrollView, TouchableOpacity, Modal } from "react-native";
-import { useEffect, useRef, useState } from "react";
-import { MaterialIcons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import CreateWorkoutForm from '../components/CreateWorkoutForm';
-import { useWorkoutContext } from '../../hooks/useWorkoutContext';
+import {useState} from "react"
+import { View,Text, TouchableOpacity } from "react-native"
+import SignupModal from "../components/SignupModal.js"
+import LoginModal from "../components/LoginModal.js";
+import {useLogout} from "../../hooks/useLogout.js"
+import {useAuthContext} from "../../hooks/useAuthContext.js"
 
-// components
-import WorkoutInfo from '../components/WorkoutInfo';
+const Account = () => {
+    const [signupModalState, setSignupModalState] = useState(false);
+    const [loginModalState, setLoginModalState] = useState(false);
+    const {logout} = useLogout()
+    const {user} = useAuthContext()
 
-const WorkoutsPage = () => {
-    const {workouts, dispatch} = useWorkoutContext();
-    const [modal, setModal] = useState(false);
-
-
-    // render workouts when first renders
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            try{
-                const response = await fetch('http://10.0.2.2:3000/api/GymPal/workouts/');
-                const workoutsJson = await response.json();
-
-                if (response.ok)
-                {
-                    dispatch({type: 'SET_WORKOUTS', payload: workoutsJson})
-                }
-            }
-            catch(err) {
-                console.log(err)
-            }
-        }
-
-        fetchWorkouts();
-    }, [dispatch]);
-
-    const scrollRef = useRef();
-    const onPressTouch = () => {
-        scrollRef.current?.scrollTo({
-          y: 0,
-          animated: true,
-        });
+    const handleLogout = () => {
+        logout()
     }
 
-    return (
-            <View className='flex-1 bg-neutral-800'>
-                    
-                <Modal visible={modal} animationType='fade' transparent={true}>
-                    <View className='flex-1 bg-[#000000aa]'>
-                        <View className='m-5 bg-white rounded-2xl mt-40'>
-                            <TouchableOpacity onPress={() => setModal(false)}>
-                                <View className='items-center mt-2'>
-                                    <MaterialCommunityIcons name="close-circle-outline" size={24} color="black" />
-                                </View>
-                            </TouchableOpacity>
-                            <CreateWorkoutForm setModal={setModal}/>
-                        </View>
-                    </View>
-                </Modal>
-                
-                <ScrollView ref={scrollRef}>
+    return(
+        <View className='flex-1 bg-neutral-800' >
+            <SignupModal visible={signupModalState} signupModalState={signupModalState} setSignupModalState={setSignupModalState}/>
+            <LoginModal visible={loginModalState} loginModalState={loginModalState} setLoginModalState={setLoginModalState} />       
 
-                    <TouchableOpacity onPress={() => setModal(true)}>
-                        <View className='absolute right-2 top-2'>
-                            <MaterialIcons name="add-circle-outline" size={24} color="white" />
-                        </View>
+                {user && (<View className='justify-center items-center flex-1' >
+                    <Text className='text-white text-lg'>User: {user.email}</Text>
+                    <TouchableOpacity className='m-5 w-20 h-10 border border-stone-300 rounded-md justify-center items-center' onPress={handleLogout}>
+                        <Text className='text-white text-xl'>
+                            Logout
+                        </Text>
+
                     </TouchableOpacity>
-
-                    <View className="mt-8"></View>
-                    {workouts && workouts.map((workout) => (
-                        <WorkoutInfo key={workout._id} workout={workout} />
-                    ))}
-
-                </ScrollView>
-
-                <TouchableOpacity onPress={onPressTouch}>
-                    <View className='absolute right-10 bottom-10'>
-                        <FontAwesome5 name="arrow-circle-up" size={26} color='white'/>
                     </View>
+                )}
+                {!user && (<View className='justify-center items-center flex-1'>
+                <TouchableOpacity className='m-5 w-20 h-10 border border-stone-300 rounded-md justify-center items-center' onPress={() => setLoginModalState(true)}>
+                    <Text className='text-white text-xl'>
+                        Login
+                    </Text>
                 </TouchableOpacity>
+                <TouchableOpacity className='m-5 w-20 h-10 border border-stone-300 rounded-md justify-center items-center' onPress= {() => setSignupModalState(true)}>
+                    <Text className='text-white text-xl'>
+                        Signup
+                    </Text>
+                </TouchableOpacity>
+                </View>
+                )}
 
-            </View>
-    );
-};
+       
+        </View>
+    )
+}
 
-export default WorkoutsPage;
+export default Account;
